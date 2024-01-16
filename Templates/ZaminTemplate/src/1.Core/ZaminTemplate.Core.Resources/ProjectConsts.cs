369e66d210a9ff69a28fce4ed28ae23c8193b4af
oroumand <ar.oroumand@gmail.com>
2024-01-16 17:29:03 +0330

    update to version 8

---
+++ Templates/ZaminTemplate/src/1.Core/ZaminTemplate.Core.Resources/ProjectConsts.cs
@@ -0,0 +1,21 @@
+﻿namespace ZaminTemplate.Core.Resources;
+
+public sealed class ProjectConsts
+{
+    public const byte ID_MIN_VALUE = 1;
+    public const byte ID_MIN_LENGTH = 2;
+    public const byte ID_MAX_LENGTH = 200;
+    public const byte PRICE_MIN_VALUE = 0;
+
+    public const byte NAME_MIN_LENGTH = 2;
+    public const byte NAME_MAX_LENGTH = 250;
+
+
+    public const byte DESCRIPTION_MIN_LENGTH = 50;
+    public const short DESCRIPTION_MAX_LENGTH = 500;
+
+
+    public const byte URI_MIN_LENGTH = 10;
+    public const short URI_MAX_LENGTH = 1000;
+
+}
---
+++ Templates/ZaminTemplate/src/1.Core/ZaminTemplate.Core.Resources/ProjectTranslation.cs
@@ -0,0 +1,17 @@
+﻿namespace ZaminTemplate.Core.Resources;
+
+public sealed class ProjectTranslation
+{
+    public const string ID = nameof(ID);
+    public const string BUSINESS_ID = nameof(BUSINESS_ID);
+    public const string SOFTWAREPART_ID = nameof(SOFTWAREPART_ID);
+
+
+    public const string FIRST_NAME = nameof(FIRST_NAME);
+    public const string LAST_NAME = nameof(LAST_NAME);
+    public const string DESCRIPTION = nameof(DESCRIPTION);
+    public const string IMAGE_URL = nameof(IMAGE_URL);
+
+
+
+}
